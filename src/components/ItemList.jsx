0ee38982ca1d32733d6be2d2f676a48b28768a08
Carla Villagra <villagraconce06@gmail.com>
2023-10-23 20:23:55 -0300

    no muestra categoria

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import Item from './Item'
 
-const ItemList = ({productos}) => {
-  return (
-    <div className='cajitas'>
-        {
-            productos.map((p) =>{
-                return(
-                    <Item
-                    id={p.id}
-                    key={p.id}
-                    nombre={p.nombre}
-                    descripcion={p.descripcion}
-                    categoria={p.categoria}
-                    precio={p.precio}
-                    />
-                )
-            })
-        }
-    </div>
-)
+const ItemList = ({ productos }) => {
+
+    return (
+
+        <div className='cajitas'>
+
+            {
+                productos.map((p) => {
+
+                    return (
+                        <Item
+                            id={p?.id}
+                            key={p?.id}
+                            nombre={p?.nombre}
+                            descripcion={p?.descripcion}
+                            categoria={p?.categoria}
+                            precio={p?.precio}
+                        />
+
+                    )
+
+                })
+
+            }
+
+        </div>
+
+    )
+
 }
 
 export default ItemList
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,33 +2,21 @@ import { useParams } from 'react-router-dom'
 import ItemList from './ItemList'
 import { useEffect, useState } from 'react'
 
-
 const ItemListContainer = () => {
 
-  const [product, setProduct] = useState( productos = [
-    { id: 1, nombre: "Campera ", descripcion: "excelente estado. Ningun detalle, es rompeviento", talle: "M", categoria: "Hombre", precio: 12000 },
-    { id: 2, nombre: "Conjunto", descripcion: "en buen estado, solo desgaste de color, lo demas impecable", talle: "L", categoria: "Mujer", precio: 15000 },
-    { id: 3, nombre: "Remera", descripcion: "usado , con algunas manchitas minimas", talle: "M", categoria: "Mujer", precio: 24000 },
-    { id: 4, nombre: "Campera", descripcion: "con detalles, solo hay que arreglar el cierre", talle: "S", categoria: "Hombre", precio: 45000 },
-    { id: 5, nombre: "Visera", descripcion: "nuevo. Visera polo ralph ", talle: "regulable", categoria: "Accesorios", precio: 3000 },
-  ])
-
+  const [ producto, setProducto] = useState([])
   const { categoria } = useParams()
 
-
-  useEffect(() => {
-    const asynFunc = categoria ? filterCategoria : mostrarProductos;
-
-    asynFunc(categoria)
-    .then(response =>{
-      setProduct(response)
-    })
-    .catch(error =>{
-      console.error(error)
-    })
-  }, [categoria])
-
+console.log(categoria)
   
+  const productos = [
+    {id:1 , nombre: "Campera", descripcion:"excelente estado. Ningun detalle, es rompeviento", talle:"M", categoria:"hombre", precio: 12000},
+    {id:2 , nombre: "Conjunto", descripcion:"en buen estado, solo desgaste de color, lo demas impecable" , talle:"L", categoria:"mujer",  precio: 15000},
+    {id:3 , nombre: "Remera", descripcion:"usado , con algunas manchitas minimas" , talle:"M",  categoria:"mujer", precio: 24000},
+    {id:4 , nombre: "Campera", descripcion:"con detalles, solo hay que arreglar el cierre" , talle:"S" , categoria:"hombre", precio: 45000},
+    {id:5 , nombre: "Visera", descripcion:"nuevo. Visera polo ralph " , talle:"regulable" , categoria:"accesorios", precio: 3000}
+]
+
 
   const mostrarProductos = new Promise((resolve, reject) => {
 
@@ -40,17 +28,19 @@ const ItemListContainer = () => {
     } else {
       reject("No se encontraron productos")
     }
+
   })
+  const filterCategoria = productos.filter((producto) => producto.categoria == categoria)
   
-  const filterCategoria = productos.filter((product) => product.categoria === categoria)
 
   return (
+
     <div>
-
-      <ItemList product={product} />
-
+    { categoria ? <ItemList producto={filterCategoria} /> : <ItemList productos={productos} />}  
     </div>
+
   )
-} 
+
+}
 
 export default ItemListContainer
